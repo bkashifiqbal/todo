38ed59e0710f504fb494aca64bf39df1a7dce7fb
kashif <bkashifiqbal@gmail.com>
2021-08-18 12:59:37 +0500

    Add type to onKeyPress funciton parameter

--- Components/TodoItem.tsx
+++ Components/TodoItem.tsx
@@ -28,7 +28,7 @@ const TodoItem = ({todo, onSubmit}: TodoItemProps) => {
         }
     }, [])
 
-    const onKeyPress=({nativeEvent})=>{
+    const onKeyPress=({nativeEvent}:any)=>{
         if(nativeEvent.key==='Backspace' && content===''){
             console.log('Deleted')
         }
